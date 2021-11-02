336852f42effdb09cff33fda1423d155ef9a88a1
thanhloc1506 <locpooh1506@gmail.com>
2021-11-02 09:22:31 +0700

    change cloud db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,25 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {MongooseModule} from "@nestjs/mongoose";
-import {AuthModule} from './auth/auth.module';
-import {UserModule} from './user/user.module';
-import {DatabaseModule} from './database/database.module';
-import {ConfigModule} from "@nestjs/config";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [
-        MongooseModule.forRoot('mongodb://localhost/nestjs-test'),
-        AuthModule,
-        UserModule,
-        DatabaseModule,
-        ConfigModule.forRoot({
-            ignoreEnvFile: true
-        })
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://bk4l:bk4l@bk4l.nrrll.mongodb.net/nestjs-test?retryWrites=true&w=majority',
+    ),
+    AuthModule,
+    UserModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/config/mongodb.config.ts
+++ src/config/mongodb.config.ts
@@ -1,6 +1,8 @@
-import {registerAs} from "@nestjs/config";
+import { registerAs } from '@nestjs/config';
 export default registerAs('mongodb', () => {
-    return {
-        uri: process.env.MONGO_URI || "mongodb://localhost:27017/nestjs-test"
-    };
+  return {
+    uri:
+      process.env.MONGO_URI ||
+      'mongodb+srv://bk4l:bk4l@bk4l.nrrll.mongodb.net/nestjs-test?retryWrites=true&w=majority',
+  };
 });
